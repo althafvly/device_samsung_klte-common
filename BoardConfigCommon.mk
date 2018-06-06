6c4d544697c30dae1bed5dba34165a7dcd558555
althafvly <althafvly@gmail.com>
2018-06-06 05:51:50 +0000

    klte-common: Add IMS support

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,6 +36,10 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 
+# Fs configs
+TARGET_FS_CONFIG_GEN := \
+      $(DEVICE_PATH)/configs/file_caps.fs
+
 # Extended Filesystem Support
 TARGET_EXFAT_DRIVER := sdfat
 
